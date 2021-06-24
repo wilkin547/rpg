b7c9a8391c5e4e31f91cc686ba6f3a812c8d19f2
bryantchacon <bryantneta@hotmail.com>
2021-06-23 20:27:01 -0500

    Refactorizacion del script del script del player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 4.0f;
-    private float currentSpeed;
 
-    private bool walking = false; //Variable que se refiere al valor del bool Walking, parametro del Animator
+    private bool walking = false; //walking empieza en false porque el player esta inmovil al iniciar el juego
     public Vector2 lastMovement = Vector2.zero; //Variable 2d que se refiere a que el player esta inmovil(por .zero) al inicio, y el valor que se le dara sera en que direccion queda mirando al detenerse
 
     private const string horizontal = "Horizontal"; //Variable que guarda el input Horizontal de unity y al parecer tambien se usa al referirse al parametro Horizontal del Animator
@@ -48,56 +47,35 @@ public class PlayerController : MonoBehaviour
     {
         //Formula del movimiento: s = v * t; espacio a moverse = velocidad * tiempo
 
-        walking = false; //Walking es false todo el timepo, a menos que alguno o ambos de los siguientes ifs se este cumpliendo(porque esto esta en el Update), si no, volvera a ser false
+        walking = false; //Walking es false todo el timepo, a menos que se detecte movimiento
 
         if (Input.GetMouseButtonDown(0)) //Si se presiona el click izquierdo del mouse...
         {
             attacking = true; //attacking se vuelve true
             attackTimeCounter = attackTime; //attackTimeCounter valdra lo que se halla indicado en attackTime desde el editor
-            playerRigidBody.velocity = Vector2.zero; //El player se detendra
+            playerRigidBody.velocity = Vector2.zero; //El player se detendra si se estaba moviendo
             animator.SetBool(attackingState, true); //Y se reproducira la animacion de ataque
         }
 
         if (attacking) //Si attacking es true...
         {
-            attackTimeCounter -= Time.deltaTime; //El contador del tiempo de ataque se reduce hasta que llega a 0 y...
+            attackTimeCounter -= Time.deltaTime; //El contador del tiempo que dura el ataque se ira reduciendo hasta llegar a 0 y...
 
             if (attackTimeCounter < 0) //Si attackTimeCounter es menor que 0...
             {
                 attacking = false; //attacking se vuelve false
-                animator.SetBool(attackingState, false); //Y attacking en el animator se vuelve false
+                animator.SetBool(attackingState, false); //Y la animacion de ataque se detiene
             }
         }
-        else
+        else //Si no...
         {
-            if (Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f) //Si el movimiento absoluto(por Math.Abs) en valor horizontal del input es mayor que 0.5(presion en el boton), o sea, si se detecta input de manera horizontal...
+            if (Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f || Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f) //Si se detecta movimiento en cualquiera o ambos vectores...
             {
-                /*
-                //NOTA: Este codigo se comenta porque se pasa de mover al player con coordenadas usando translate a moverlo usando las fisicas del motor con el rigid body
-                this.transform.Translate(new Vector3(Input.GetAxisRaw(horizontal) * speed * Time.deltaTime, 0, 0)); //... mueve al player(por this., porque este script esta en el player) en horizontal...
-                */
+                walking = true; //walking se vuelve true
 
-                playerRigidBody.velocity = new Vector2(Input.GetAxisRaw(horizontal) * currentSpeed, playerRigidBody.velocity.y); //Mueve al player en horizontal, es Vector2 porque el rigid body es Rigidbody2D, la coordenada en "x" sera la que detecte en el input horizontal, multiplicado por la velocidad, la de "y" no se indica por si tambien se esta moviendo en "y", o sea, en diagonal, todo esto con .velocity en playerRigidBody
-                walking = true; //... walking se vuelve verdadero mientras el player se mueva...
-                lastMovement = new Vector2(Input.GetAxisRaw(horizontal), 0); //... y guarda la direccion en la que el player se detiene
-            }
+                lastMovement = new Vector2(Input.GetAxisRaw(horizontal), Input.GetAxisRaw(vertical)); //lastMovement sera el valor que obtenga del input de los vectores
 
-            if (Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f)
-            {
-                //this.transform.Translate(new Vector3(0, Input.GetAxisRaw(vertical) * speed * Time.deltaTime, 0));
-
-                playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, Input.GetAxisRaw(vertical) * currentSpeed);
-                walking = true;
-                lastMovement = new Vector2(0, Input.GetAxisRaw(vertical));
-            }
-
-            if (Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f && Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f) //Si detecta movimiento en ambos vectores, o sea, un momiviento vertical...
-            {
-                currentSpeed = speed / Mathf.Sqrt(2); //currectSpeed es igual a speed entre raiz cuadrada de 2, porque estara duplicada por ambos vectores
-            }
-            else //Si no, si solo hay movimiento en un vector...
-            {
-                currentSpeed = speed; //currentSpeed sera igual al speed original
+                playerRigidBody.velocity = lastMovement.normalized * speed; //Y el movimiento del player sera lastMovement(el input de los vectores) normalizado, multiplicado por la velocidad
             }
         }
 
